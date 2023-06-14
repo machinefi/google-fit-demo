ccc5305ddfe961b6999a02d9df39e9f0e5b4ecad
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-06-14 14:12:04 -0300

    add session provider

--- adapter/app/providers.tsx
+++ adapter/app/providers.tsx
@@ -1,9 +1,21 @@
 "use client";
 
 import { WagmiConfig } from "wagmi";
+import { SessionProvider } from "next-auth/react";
 
 import wagmiCongig from "@/features/web3/services/wagmi/client";
+import { Session } from "next-auth";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <WagmiConfig config={wagmiCongig}>{children}</WagmiConfig>;
+export default function Providers({
+  children,
+  session,
+}: {
+  children: React.ReactNode;
+  session: Session | null;
+}) {
+  return (
+    <SessionProvider session={session}>
+      <WagmiConfig config={wagmiCongig}>{children}</WagmiConfig>
+    </SessionProvider>
+  );
 }
