e8d307955206d187cca72d37f49bbbabd2a914ab
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-06-15 17:13:44 -0300

    Create constants.ts

---
+++ blockchain/constants.ts
@@ -0,0 +1,3 @@
+export const SBT_CONTRACT_NAME = process.env.SBT_CONTRACT_NAME || "GFitDevice";
+export const SBT_CONTRACT_SYMBOL = process.env.SBT_CONTRACT_SYMBOL || "GFD";
+export const SBT_URI = process.env.SBT_URI || "";
