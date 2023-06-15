b9f9061d33decd151d59d9d84949295aff6d3194
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-06-15 15:00:27 -0300

    remove clutter

--- adapter/features/secrets/services/redis/store.ts
+++ adapter/features/secrets/services/redis/store.ts
@@ -1,17 +1,6 @@
 import "server-only";
 
 import redisCli from "./client";
-import { EncryptedToken } from "../../types/EncryptedToken";
-
-export const store = async (key: string, value: EncryptedToken) => {
-  const tx = redisCli.multi();
-  tx.hset(key, {
-    iv: value.iv,
-    encryptedData: value.encryptedData,
-  });
-
-  return tx.exec();
-};
 
 export const storeString = async (key: string, value: string) => {
   await redisCli.set(key, value);
