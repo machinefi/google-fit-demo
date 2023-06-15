05119fd05c7b6ab38d179bb140bfcaa494ac81bc
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-06-15 19:32:08 -0300

    update configs

--- blockchain/constants.ts
+++ blockchain/constants.ts
@@ -1,6 +1,6 @@
-export const SBT_CONTRACT_NAME = process.env.SBT_CONTRACT_NAME || "GFitDevice";
-export const SBT_CONTRACT_SYMBOL = process.env.SBT_CONTRACT_SYMBOL || "GFD";
+export const SBT_CONTRACT_NAME = process.env.SBT_CONTRACT_NAME || "Device SBT";
+export const SBT_CONTRACT_SYMBOL = process.env.SBT_CONTRACT_SYMBOL || "DSBT";
 export const SBT_URI = process.env.SBT_URI || "";
 
-export const REWARDS_CONTRACT_NAME = process.env.REWARDS_CONTRACT_NAME || "DeviceRewards";
+export const REWARDS_CONTRACT_NAME = process.env.REWARDS_CONTRACT_NAME || "Device Rewards";
 export const REWARDS_URI = process.env.REWARDS_URI || "";
