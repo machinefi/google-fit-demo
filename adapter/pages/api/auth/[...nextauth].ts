310454f299a9a93bebf54e7ae49f7ebf9bec442e
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-06-15 14:53:35 -0300

    update authentication scopes

--- adapter/pages/api/auth/[...nextauth].ts
+++ adapter/pages/api/auth/[...nextauth].ts
@@ -7,8 +7,25 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID || "",
       clientSecret: process.env.GOOGLE_SECRET || "",
+      authorization: {
+        params: {
+          scope: "openid email profile https://www.googleapis.com/auth/fitness.activity.read",
+        },
+      },
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
