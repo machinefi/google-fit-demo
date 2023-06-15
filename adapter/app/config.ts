93c32b2b34ed998aa186c75b18b2bae69a9b57c3
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-06-15 14:51:21 -0300

    Update config.ts

--- adapter/app/config.ts
+++ adapter/app/config.ts
@@ -1,5 +1,5 @@
-export const OURA_BASE_URL = "https://api.ouraring.com";
 export const DEMO_BASE_URL = "https://oura-adapter.vercel.app";
+export const GOOGLE_FIT_BASE_URL = "https://www.googleapis.com/fitness/v1/users/me";
 
 export const DEVICE_TOKEN = process.env.DEVICE_TOKEN ?? "";
 export const HTTP_ROUTE = process.env.HTTP_ROUTE ?? "";
