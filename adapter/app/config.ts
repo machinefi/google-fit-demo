430357cb436f52d71ebfe64fb4dc00817a495922
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-08-21 23:31:55 -0300

    Update config.ts

--- adapter/app/config.ts
+++ adapter/app/config.ts
@@ -1,4 +1,4 @@
 export const GOOGLE_FIT_BASE_URL = "https://www.googleapis.com/fitness/v1/users/me";
 
-export const DEVICE_TOKEN = process.env.DEVICE_TOKEN ?? "";
+export const API_TOKEN = process.env.API_TOKEN ?? "";
 export const HTTP_ROUTE = process.env.HTTP_ROUTE ?? "";
